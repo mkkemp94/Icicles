14ecf98245a1ea610db43783784e329cec6de49a
Michael Kemp <mkemp@accu-time.com>
2018-02-26 08:24:56 -0500

    Initial commit + stub classes

---
+++ core/src/com/udacity/gamedev/icicles/IciclesGame.java
@@ -0,0 +1,11 @@
+package com.udacity.gamedev.icicles;
+
+import com.badlogic.gdx.Game;
+
+public class IciclesGame extends Game {
+	
+	@Override
+	public void create () {
+		// TODO : Create screen
+	}
+}
