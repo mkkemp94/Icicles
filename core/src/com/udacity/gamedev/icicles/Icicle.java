3fc0c9e1cc52999d479afbe547218eda88d235cd
Michael Kemp <mkemp@accu-time.com>
2018-02-26 11:38:06 -0500

    Trying to center icicle...

--- core/src/com/udacity/gamedev/icicles/Icicle.java
+++ core/src/com/udacity/gamedev/icicles/Icicle.java
@@ -11,9 +11,10 @@ public class Icicle {
 
     private Vector2 position;
 
-    public Icicle() {
+    public Icicle(Vector2 startPos) {
         // TODO : Randomize position
-        position = new Vector2(120, 150);
+//        position = new Vector2(120, 150);
+        position = startPos;
     }
 
     public void render(ShapeRenderer renderer) {
--- core/src/com/udacity/gamedev/icicles/IciclesScreen.java
+++ core/src/com/udacity/gamedev/icicles/IciclesScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 /**
@@ -19,15 +20,19 @@ public class IciclesScreen implements Screen {
     @Override
     public void show() {
         renderer = new ShapeRenderer();
+        renderer.setAutoShapeType(true);
         viewport = new ExtendViewport(Constants.WORLD_SIZE, Constants.WORLD_SIZE);
-        icicle = new Icicle();
+        icicle = new Icicle(new Vector2(Constants.WORLD_SIZE / 2, Constants.WORLD_SIZE / 2));
     }
 
     @Override
     public void render(float delta) {
         viewport.apply();
+
         Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.setProjectionMatrix(viewport.getCamera().combined);
 
         renderer.begin(ShapeRenderer.ShapeType.Filled);
 //        renderer.triangle(50, 20, 70, 30, 50, 40);
@@ -37,7 +42,7 @@ public class IciclesScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(Constants.WORLD_SIZE, Constants.WORLD_SIZE);
+        viewport.update(Constants.WORLD_SIZE, Constants.WORLD_SIZE, true);
     }
 
     @Override
